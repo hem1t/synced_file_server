643b62009e258c54bf80bfe26178aa5f7ea6a18c
hem1t <sunny10fb@gmail.com>
2023-08-26 19:57:43 +0530

    tested "FTree" with as_string()

--- common_tools/file_tree/src/lib.rs
+++ common_tools/file_tree/src/lib.rs
@@ -15,22 +15,24 @@ impl FTree {
 
     pub fn from_path(path: PathBuf) -> Result<FTree, String> {
         let mut root = FTree::new(path.clone());
-        if path.exists() && path.is_dir() {
-            let entries = path.read_dir();
-            if entries.is_err() {
-                return Err(format!("Can't read {:?}", path));
-            }
+        if path.exists() {
+            if path.is_dir() {
+                let entries = path.read_dir();
+                if entries.is_err() {
+                    return Err(format!("Can't read {:?}", path));
+                }
 
-            for entry in entries.unwrap() {
-                if let Ok(entry) = entry {
-                    let child = FTree::from_path(entry.path());
-                    if let Ok(child) = child {
-                        root.children.push(Box::new(child));
+                for entry in entries.unwrap() {
+                    if let Ok(entry) = entry {
+                        let child = FTree::from_path(entry.path());
+                        if let Ok(child) = child {
+                            root.children.push(Box::new(child));
+                        } else {
+                            return child;
+                        }
                     } else {
-                        return child;
+                        return Err(format!("{:?} broken at: {:?}", entry, path));
                     }
-                } else {
-                    return Err(format!("{:?} broken at: {:?}", entry, path));
                 }
             }
             return Ok(root);
@@ -46,6 +48,21 @@ impl FTree {
     pub fn is_file(&self) -> bool {
         return self.path.is_file();
     }
+
+    pub fn as_string(&self) -> String {
+        let mut tree = String::from(self.path.to_str().unwrap());
+        if self.has_children() {
+            for child in &self.children {
+                tree.push_str(&child.as_string());
+                tree.push(',');
+            }
+        }
+        return tree;
+    }
+
+    fn has_children(&self) -> bool {
+        !&self.children.is_empty()
+    }
 }
 
 #[cfg(test)]
@@ -53,5 +70,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_ftree_from_path() {}
+    fn test_ftree_from_path() {
+        assert_eq!(
+            "./examples_dir./examples_dir/empty_dir,./examples_dir/test_dir./examples_dir/test_dir/a_dir./examples_dir/test_dir/a_dir/with_file,,./examples_dir/test_dir/hello_file,./examples_dir/test_dir/second_file,,",
+            FTree::from_path(PathBuf::from("./examples_dir"))
+                .unwrap()
+                .as_string()
+        );
+    }
 }
