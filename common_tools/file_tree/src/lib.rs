3b8cd8b874feb126cc7213946600891eb2874a16
hem1t <sunny10fb@gmail.com>
2024-01-01 07:09:38 +0530

    BIG CHANGE
    realized can read whole `FTree` as a list instead of Tree Structure.

--- common_tools/file_tree/src/lib.rs
+++ common_tools/file_tree/src/lib.rs
@@ -1,121 +1,129 @@
+#![allow(unused, dead_code)]
 use std::{
     fs, io,
     path::PathBuf,
+    str::FromStr,
     time::{Duration, SystemTime},
 };
 
-#[derive(Debug)]
-pub enum FTreeErr {
-    NotExists(PathBuf),
-    NotReadable(PathBuf),
-    NotWritable(PathBuf),
-    IOErr(io::Error),
-}
-
-pub struct FTree {
+#[derive(Clone)]
+pub struct FNode {
     path: PathBuf,
-    children: Vec<Box<FTree>>,
+    is_f: bool,
+    modified: Duration,
 }
 
 // This impl should hold private fns
-impl FTree {
-    fn has_children(&self) -> bool {
-        !&self.children.is_empty()
-    }
-}
-
-#[allow(dead_code)]
-impl FTree {
-    fn new(path: PathBuf) -> Self {
-        Self {
-            path,
-            children: Vec::new(),
-        }
-    }
-
-    ///
-    /// Purpose: To create `FTree` recursively.
-    ///
-    pub fn from_path(path: PathBuf) -> Result<Self, FTreeErr> {
-        let mut root = Self::new(path.clone());
-        if path.exists() {
-            if path.is_dir() {
-                let entries = path.read_dir();
-                if entries.is_err() {
-                    return Err(FTreeErr::NotReadable(path.clone()));
-                }
-
-                for entry in entries.unwrap() {
-                    if let Ok(entry) = entry {
-                        let child = Self::from_path(entry.path());
-                        if let Ok(child) = child {
-                            root.children.push(Box::new(child));
-                        } else {
-                            return child;
-                        }
-                    } else {
-                        return Err(FTreeErr::IOErr(entry.err().unwrap()));
-                    }
-                }
-            }
-            return Ok(root);
-        } else {
-            return Err(FTreeErr::NotExists(path.clone()));
-        }
-    }
-
-    ///
-    /// Purpose: To create `FTree` recursively, out of string.
-    ///
-    pub fn from_string(tree: String) -> Result<Self, FTreeErr> {
-        todo!()
+impl FNode {
+    fn new(path: &PathBuf) -> Result<Self, io::Error> {
+        Ok(Self {
+            path: path.clone(),
+            is_f: path.is_file(),
+            modified: FNode::node_time(path)?,
+        })
     }
 
     ///
     /// Purpose: To return a time for the file or directory to check if
     ///          it was recently modified.
     ///
-    fn file_time(&self) -> Result<Duration, FTreeErr> {
-        if let Ok(meta) = fs::metadata(self.path.clone()) {
-            if let Ok(time) = meta.modified() {
-                return Ok(time.duration_since(SystemTime::UNIX_EPOCH).unwrap());
+    fn node_time(path: &PathBuf) -> Result<Duration, io::Error> {
+        let meta = fs::metadata(path.clone())?;
+        if let Ok(time) = meta.modified() {
+            return Ok(time.duration_since(SystemTime::UNIX_EPOCH).unwrap());
+        } else {
+            eprintln!(
+                "[Warning] Your platform doesn't supports File Time, so, considering file as new!"
+            );
+            return Ok(SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap());
+        }
+    }
+
+    fn to_string(&self) -> String {
+        format!(
+            "{}:{}:{}",
+            self.path.to_string_lossy(),
+            self.modified.as_secs(),
+            if self.is_f { 'f' } else { 'd' }
+        )
+    }
+
+    fn from_string(s: String) -> Result<Self, io::Error> {
+        let mut n = s.split(':');
+        Ok(Self {
+            path: PathBuf::from_str(n.next().unwrap()).unwrap(),
+            modified: Duration::from_secs(n.next().unwrap().parse::<u64>().unwrap()),
+            is_f: n.next().unwrap() == "f",
+        })
+    }
+}
+
+struct FTree {
+    nodes: Vec<FNode>,
+    // largest avail Duration.
+    modified: Duration,
+}
+
+impl FTree {
+    pub fn new(path: PathBuf) -> Result<FTree, io::Error> {
+        let mut ftree = Self {
+            nodes: vec![FNode::new(&path)?],
+            modified: Duration::from_secs(0),
+        };
+        ftree.from_path(path)?;
+        Ok(ftree)
+    }
+
+    fn add(&mut self, node: FNode) {
+        self.nodes.push(node.clone());
+        // larger modified time;
+        if self.modified < node.modified {
+            self.modified = node.modified;
+        }
+    }
+
+    ///
+    /// Purpose: To list all file and dirs
+    ///
+    fn from_path(&mut self, path: PathBuf) -> Result<(), io::Error> {
+        let entries = path.read_dir()?;
+        for entry in entries {
+            if let Ok(e) = entry {
+                let e = e.path();
+                self.add(FNode::new(&e)?);
+                if e.is_dir() {
+                    self.from_path(e)?;
+                }
             } else {
-                eprintln!(
-                    "[Warning] Your platform doesn't supports File Time, so, considering file as new!"
-                );
-                return Ok(SystemTime::now()
-                    .duration_since(SystemTime::UNIX_EPOCH)
-                    .unwrap());
+                return Err(entry.err().unwrap());
             }
         }
-        return Err(FTreeErr::NotReadable(self.path.clone()));
+        Ok(())
     }
 
     ///
     /// Purpose: To return unique string for the FTree Structure,
     ///          which will have unique hash value, when calculated.
     ///
-    pub fn as_string(&self) -> Result<String, FTreeErr> {
-        let mut tree = String::from(self.path.to_str().unwrap());
-        tree.push(':');
-        tree.push_str(&self.file_time()?.as_secs().to_string().as_str());
-
-        if self.has_children() {
-            tree.push(',');
-            for child in &self.children {
-                tree.push_str(&child.as_string()?);
-            }
-        } else {
-            tree.push(';');
-        }
-        return Ok(tree);
+    pub fn as_string(&self) -> Result<String, io::Error> {
+        Ok(self
+            .nodes
+            .iter()
+            .map(|n| format!("{},", n.to_string()))
+            .collect::<String>())
     }
 
-    ///
-    /// Purpose: Create file structure using `FTree`.
-    ///
-    pub fn write_to_path(_path: PathBuf) -> Result<(), FTreeErr> {
-        todo!()
+    pub fn from_string(s: String) -> Result<FTree, io::Error> {
+        let mut ftree = FTree {
+            nodes: Vec::new(),
+            modified: Duration::from_secs(0),
+        };
+        for node in s.split(',') {
+            ftree.add(FNode::from_string(node.to_string())?);
+        }
+        Ok(ftree)
     }
 }
 
@@ -126,8 +134,8 @@ mod tests {
     #[test]
     fn test_ftree_from_path() {
         pretty_assertions::assert_eq!(
-            "./examples_dir:1703885422,./examples_dir/test_dir:1703885422,./examples_dir/test_dir/second_file:1703885422;./examples_dir/test_dir/a_dir:1703885422,./examples_dir/test_dir/a_dir/with_file:1703885422;./examples_dir/test_dir/hello_file:1704060920;",
-            FTree::from_path(PathBuf::from("./examples_dir"))
+            "./examples_dir:1703885422:d,./examples_dir/test_dir:1703885422:d,./examples_dir/test_dir/second_file:1703885422:f,./examples_dir/test_dir/a_dir:1703885422:d,./examples_dir/test_dir/a_dir/with_file:1703885422:f,./examples_dir/test_dir/hello_file:1704060920:f,",
+            FTree::new(PathBuf::from("./examples_dir"))
                 .unwrap()
                 .as_string()
                 .unwrap()
